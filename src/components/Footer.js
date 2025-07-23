de2cb993641cb14199090ab69970f7cbd9f29e85
K8sByte <chirag.kp30@gmail,com>
2025-07-23 19:36:51 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,18 +11,20 @@ function Footer() {
   let year = date.getFullYear();
   return (
     <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-copywright">
-          <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
-            <img src={logo} alt="CKP Logo" style={{ height: "24px", width: "auto", marginRight: "20px" }} />
+      <Row className="align-items-center text-center text-md-start">
+        <Col md="6" className="footer-copywright">
+         <div style={{ display: "flex", alignItems: "center", justifyContent: "center", justifyContent: "flex-start" }}>
+            <img src={logo} alt="CKP Logo" style={{ height: "28px", width: "auto", marginRight: "10px" }} />
           </div>
         </Col>
+
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} CKP</h3>
         </Col>
+
         <Col md="6" className="footer-body">
-          <ul className="footer-icons" >
-            <li className="social-icons" >
+         <ul className="footer-icons" style={{ display: "flex", justifyContent: "flex-end", gap: "20px", paddingLeft: 0, marginBottom: 0 }}>
+            <li className="social-icons" style={{ listStyle: "none", fontSize: "1.2rem" }}>
               <a
                 href="https://github.com/chiragkp06"
                 style={{ color: "white"}}
@@ -32,7 +34,7 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
-            <li className="social-icons">
+            <li className="social-icons" style={{ listStyle: "none", fontSize: "1.2rem" }}>
               <a
                 href="https://www.linkedin.com/in/chirag-kp/"
                 style={{ color: "white"}}
